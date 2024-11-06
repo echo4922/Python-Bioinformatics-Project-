f9525237eb99fa87f4ecd91696493e005488f615
Eddie Cho, MS <112420424+echo4922@users.noreply.github.com>
2024-11-05 18:14:27 -0800

    Update Cho_OOP_FinalProj_2022.py

--- Cho_OOP_FinalProj_2022.py
+++ Cho_OOP_FinalProj_2022.py
@@ -119,15 +119,16 @@ class RNA(DNA):
         for i in range (0,len(self.sequence) - 2, 3): #Iterates through the sequence in steps of 3
             if len(self.sequence[i:i+3])==3: #Ensures the codon is of length 3
                 self.codons.append(self.sequence[i:i+3]) #Adds the codon to the list
-   
-     def translate(self):  # Method to translate the RNA sequence to a protein sequence
+
+def translate(self):  # Method to translate the RNA sequence to a protein sequence
         self.translated_sequence = ''  # Initialize an empty string for the translated sequence
-        for codon in self.codons:  #Iterate through each codon
-            if codon not in list(standard_code.keys()): #If the codon is not valid, add 'X'
+
+        for codon in self.codons:  # Iterate through each codon
+            if codon not in list(standard_code.keys()):  # If the codon is not valid, add 'X'
                 self.translated_sequence += 'X'
-            else: #Otherwise, add the corresponding amino acid
+            else:  # Otherwise, add the corresponding amino acid
                 self.translated_sequence += standard_code[codon]
-        return self.translated_sequence #Returns the translated protein sequence
+        return self.translated_sequence  # Returns the translated protein sequence
 
 #Step-5: Create Protenin class (must inherit Seq class)
 class Protein(Seq):
